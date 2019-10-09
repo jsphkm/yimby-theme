52cf0c0d830c0a2035450106c59050f6e3a9ecdc
jsphkm <jsphkm12@gmail.com>
2019-10-08 21:35:05 -0700

    Add shell script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tar -czf yimby.tar.gz ./* .??*
+scp yimby.tar.gz root@66.42.73.132:/var/www/html/wp-content/themes
+rm yimby.tar.gz
+
+ssh root@66.42.73.132 << 'ENDSSH'
+cd /var/www/html/wp-content/themes
+rm -rf yimby
+mkdir yimby
+tar xf yimby.tar.gz -C yimby
+rm yimby.tar.gz
+ENDSSH
